79cec39c79168161ecb880d164a3794c6bc4495d
Kacper Mura <kacper.mura@web.de>
2022-07-05 11:52:06 +0200

    Removed "Tägliche Impfungen" and added second booster vaccination

--- src/services/image.service.ts
+++ src/services/image.service.ts
@@ -180,31 +180,11 @@ export namespace ImageService {
                 const image = new Stats("vaccinations", background);
 
                 try {
-                    const residents = 83000000;
-                    const minResidents = (85 / 100) * residents;
-                    const requiredVaccinations = minResidents * 2;
-                    const missingVaccinations = requiredVaccinations - data.current.data.administeredVaccinations;
-
-                    const history: [] = historyData.current.data.history;
-                    let vaccinated = 0;
-                    let boosterVaccinated = 0;
-                    history.forEach((x: any) => {
-                        vaccinated += x.firstVaccination + x.secondVaccination;
-                        boosterVaccinated += x.boosterVaccination;
-                    });
-                    const r = vaccinated / 7;
-                    const daysNeeded = missingVaccinations / r;
-                    const expectedDate = new Date();
-                    expectedDate.setDate(expectedDate.getDate() + daysNeeded);
-
-                    image.setElement(numberFormat.format(data.current.data.administeredVaccinations), numberFormat.format(vaccinated + boosterVaccinated) + " die letzten 7-Tage", 960, 520, true);
-                    image.setElement(numberFormat.format(Math.round(r + (boosterVaccinated / 7))), "Heute: " + numberFormat.format(data.current.data.delta + data.current.data.secondVaccination.delta + data.current.data.boosterVaccination.delta), 265, 825);
-
-                    image.setElement(numberFormat.format(data.current.data.vaccinated), "+ ~" + numberFormat.format(data.current.data.delta) + " | " + numberFormat.format(Number((data.current.data.quote * 100).toFixed(1))) + "%", 730, 825);
-                    image.setElement(numberFormat.format(data.current.data.secondVaccination.vaccinated), "+ ~" + numberFormat.format(data.current.data.secondVaccination.delta) + " | " + numberFormat.format(Number((data.current.data.secondVaccination.quote * 100).toFixed(1))) + "%", 1195, 825);
-
-                    const boosterVaccinationQuote = data.current.data.boosterVaccination.vaccinated / residents;
-                    image.setElement(numberFormat.format(data.current.data.boosterVaccination.vaccinated), "+ ~" + numberFormat.format(data.current.data.boosterVaccination.delta) + " | " + numberFormat.format(Number((boosterVaccinationQuote * 100).toFixed(1))) + "%", 1660, 825);
+                    image.setElement(numberFormat.format(data.current.data.administeredVaccinations), "", 960, 540, true);
+                    image.setElement(numberFormat.format(data.current.data.vaccinated), "+ ~" + numberFormat.format(data.current.data.delta) + " | " + numberFormat.format(Number((data.current.data.quote * 100).toFixed(1))) + "%", 265, 825);
+                    image.setElement(numberFormat.format(data.current.data.secondVaccination.vaccinated), "+ ~" + numberFormat.format(data.current.data.secondVaccination.delta) + " | " + numberFormat.format(Number((data.current.data.secondVaccination.quote * 100).toFixed(1))) + "%", 730, 825);
+                    image.setElement(numberFormat.format(data.current.data.boosterVaccination.vaccinated), "+ ~" + numberFormat.format(data.current.data.boosterVaccination.delta) + " | " + numberFormat.format(Number((data.current.data.boosterVaccination.quote * 100).toFixed(1))) + "%", 1195, 825);
+                    image.setElement(numberFormat.format(data.current.data["2ndBoosterVaccination"].vaccinated), "+ ~" + numberFormat.format(data.current.data["2ndBoosterVaccination"].delta) + " | " + numberFormat.format(Number((data.current.data["2ndBoosterVaccination"].quote * 100).toFixed(1))) + "%", 1660, 825);
                 } catch (err) {
                     return reject();
                 }
